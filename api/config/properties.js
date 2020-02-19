a4554c522934da810385a028acbc9cfc5e9d2ab9
liu-austin <austinliu279@gmail.com>
2020-02-18 19:46:38 -0800

    changes mongo connection string

--- api/config/properties.js
+++ api/config/properties.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: 9000,
-    DB: 'mongodb://localhost:27017/fortress-panic'
+    DB: 'mongodb+srv://austinliu279:1234@cluster0-qmgnw.mongodb.net/test?retryWrites=true&w=majority'
 };
 
 // DB: 'mongodb://localhost:27017/fortress-panic'
